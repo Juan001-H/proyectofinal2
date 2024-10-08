72d1b61247ea9e87395c1b5db9f583a4d0448742
Juan001-H <herrerajuan428@gmail.com>
2024-10-07 20:40:06 -1200

    SA

--- src/main/java/co/edu/uniquindio/poo/Restaurante.java
+++ src/main/java/co/edu/uniquindio/poo/Restaurante.java
@@ -1,10 +1,37 @@
 
 package co.edu.uniquindio.poo;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class Restaurante {
     private List<Menu>menus;
     private String Nombre;
+    private List<Mesa>mesas;
+    public Restaurante( String nombre) {
+        this.menus = new LinkedList<>();
+        Nombre = nombre;
+        this.mesas = new LinkedList<>();
+    }
+    public List<Menu> getMenus() {
+        return menus;
+    }
+    public void setMenus(List<Menu> menus) {
+        this.menus = menus;
+    }
+    public String getNombre() {
+        return Nombre;
+    }
     
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+    public List<Mesa> getMesas() {
+        return mesas;
+    }
+    public void setMesas(List<Mesa> mesas) {
+        this.mesas = mesas;
+    }
+
+
 }
